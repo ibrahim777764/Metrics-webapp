ab3c8ca480201ebb544387537392e325e97ec4b0
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-07-28 14:31:22 +0500

    testing main js

--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -5,7 +5,7 @@ import store from '../redux/store/configureStore';
 import Home from '../components/main';
 
 describe('Main Component test', () => {
-  it('Renders main Page', () => {
+  it('Renders main Page', () => { 
     const app = renderer
       .create(
         <Provider store={store}>
